620b532309242ff87d8c2cc661a7f9d5c6c34436
Ovi# <covidiu97@gmail.com>
2018-11-18 22:23:34 +0100

    Added functionality

--- app.js
+++ app.js
@@ -15,31 +15,37 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/books', bookRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not found');
+app.use(function (req, res, next) {
+  const err = new Error('Not found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  if(err.status === 404) {
+  console.log(err);
+  if (err.status === 404) {
     res.render('notFound');
-  } else{
-    res.render('error');
+  } else {
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
   }
 
 });
--- routes/books.js
+++ routes/books.js
@@ -4,22 +4,34 @@ var Book = require('../models').Book;
 
 /* GET /books to show all books */
 router.get('/', (req, res, next) => {
-  Book.findAll({order: [["createdAt", "DESC"]]}).then(books => {
-    res.render('books/index', {books: books});
-  }).catch(err => res.send(500));
+  Book.findAll({
+    order: [
+      ["createdAt", "DESC"]
+    ]
+  }).then(books => {
+    res.render('books/index', {
+      books: books
+    });
+  }).catch(err => res.render('./error', {
+    message: err.message,
+    error: err
+  }));
 });
 
 // GET new book
 router.get('/new', (req, res, next) => {
-  res.render('books/new', {book: Book.build()});
+  res.render('books/new', {
+    book: Book.build()
+  });
 });
 
 // POST new book
 router.post('/new', (req, res, next) => {
   Book.create(req.body).then(book => {
-    res.redirect("/books/" + book.id);
+    res.redirect(`/books`);
+    console.log(book.id)
   }).catch(err => {
-    if(err.name === "SequelizeValidationError"){
+    if (err.name === "SequelizeValidationError") {
       res.render("books/new", {
         book: Book.build(req.body),
         errors: err.errors
@@ -27,34 +39,81 @@ router.post('/new', (req, res, next) => {
     } else {
       throw err;
     }
-  }).catch(err => res.send(500));
+  }).catch(err => res.render('./error', {
+    message: err.message,
+    error: err
+  }));
 });
 
 // GET detailed info about an individual book
-router.get('/:id', (req, res, next) => {
+router.get('/book-:id', (req, res, next) => {
   Book.findByPk(req.params.id).then(book => {
-    if(book) {
+    if (book) {
       res.render('books/update', {
-        book: book,
-        title: book.title,
-        author: book.author,
-        genre: book.genre,
-        year: book.year
+        book: book
       });
-    } 
-    
-  })
-  
+    } else {
+      res.render('./notFound');
+    }
+  }).catch(err => res.render('./error', {
+    message: err.message,
+    error: err
+  }));
 });
 
 // Edit/update the book
-router.post('/:id', (req, res, next) => {
-  res.render('books/update');
+router.post('/book-:id', (req, res, next) => {
+  Book.findByPk(req.params.id).then(book => {
+      if (book) {
+        return book.update(req.body);
+      } else {
+        res.render('./notFound');
+      }
+    }).then((book) => res.redirect('/books'))
+    // TODO: BOOK update popup
+    .catch(err => {
+      if (err.name === 'SequelizeValidationError') {
+        var book = Book.build(req.body);
+        book.id = req.params.id;
+        res.render("books/update", {
+          errors: err.errors,
+          book: book
+        });
+      } else {
+        throw err
+      }
+    }).catch(err => res.render('error', {
+      message: err.message,
+      error: err
+    }));
 });
 
+// GET delete form
+router.get('/book-:id', (req, res, next) => {
+  Book.findByPk(req.params.id).then(book => {
+    if (book) {
+      res.render('books/delete', {
+        title: book.title,
+        book: book
+      });
+    }
+  });
+});
+
+
 // Delete the book
-router.post('/:id/delete', (req, res, next) => {
-  // delete book
+router.post('/book-:id/delete', (req, res, next) => {
+  Book.findByPk(req.params.id).then(book => {
+      if (book) {
+        return book.destroy();
+      } else {
+        res.render('./notFound');
+      }
+    }).then(() => res.redirect("/books"))
+    .catch(err => res.render('error', {
+      message: err.message,
+      error: err
+    }));
 });
 
 module.exports = router;
